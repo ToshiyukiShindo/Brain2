8ba00e9cfe5620b814d4e4d2c52736d22dd5eec9
ToshiyukiShindo <fight.holding.water@gmail.com>
2022-05-14 07:23:35 +0000

    “message”

--- database/migrations/2022_05_14_113636_add_flug_to_users_table.php
+++ database/migrations/2022_05_14_113636_add_flug_to_users_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('users')) {
+            // テーブルが存在していればリターン
+            return;
+        };
+        if (Schema::hasTable('password_resets')) {
+            // テーブルが存在していればリターン
+            return;
+        };
+        if (Schema::hasTable('failed_jobs')) {
+            // テーブルが存在していればリターン
+            return;
+        };
+        if (Schema::hasTable('personal_access_tokens')) {
+            // テーブルが存在していればリターン
+            return;
+        };
+
         Schema::table('users', function (Blueprint $table) {
             $table->integer('manage_flag')->default('0');  //カラム追加
             $table->string('answer_category')->nullable();  //カラム追加
